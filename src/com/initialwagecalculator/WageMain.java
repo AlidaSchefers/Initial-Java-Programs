3aae4e669dbfeb6727e1ecac5cc6f70036b0f3cb
AlidaSchefers <alidadschefers@gmail.com>
2021-03-11 22:14:25 -0500

    added comment

--- src/com/initialwagecalculator/WageMain.java
+++ src/com/initialwagecalculator/WageMain.java
@@ -1,10 +1,10 @@
 package com.initialwagecalculator;
 
 public class WageMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //main method is declared static to enable Java runtime to directly call this method without having to create a new object.
         var employee = new Employee(
                 50_000, 20);
-        System.out.println(Employee.numberOfEmployees);
+        Employee.printNumberOfEmployees();
         int wage = employee.calculateWage();
         System.out.println(wage);
     }
