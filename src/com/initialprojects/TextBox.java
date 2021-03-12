017b553b55243d36d49082e7326fc964ec246bc7
AlidaSchefers <alidadschefers@gmail.com>
2021-03-11 22:12:30 -0500

    added constructor and methods

--- src/com/initialprojects/TextBox.java
+++ src/com/initialprojects/TextBox.java
@@ -1,2 +1,17 @@
-package com.initialprojects;public class TextBox {
+package com.initialprojects;
+
+public class TextBox extends UIControl {
+    private String text = "";
+
+    public TextBox() {
+        super(true);
+        System.out.println("TextBox");
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    public void clear() {
+        text = "";
+    }
 }
