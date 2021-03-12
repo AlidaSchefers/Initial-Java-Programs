bd87a77b4518d6c0280332f2d3ff24fb09d2f5af
AlidaSchefers <alidadschefers@gmail.com>
2021-03-11 22:09:53 -0500

    initial commit

---
+++ src/com/initialprojects/Main.java
@@ -0,0 +1,7 @@
+package com.initialprojects;
+
+public class Main {
+    public static void main(String[] args) {
+        var box1 = new TextBox();
+    }
+}
---
+++ src/com/initialprojects/TextBox.java
@@ -0,0 +1,2 @@
+package com.initialprojects;public class TextBox {
+}
